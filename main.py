3af0a2facedd7970ba5d4214f2aa949dbbbf0d83
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-07 21:01:00 +0300

    show image

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ import cv2 as cv
 if __name__ == '__main__':
     # Load an color image in grayscale
     img = cv.imread('data/r3.jpg', 0)
+    cv.imshow('image', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
