9898416d22f42e2353772f85a27a2ad107c2d8d0
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-07 21:09:47 +0300

    writing image

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import cv2 as cv
 if __name__ == '__main__':
     # Load an color image in grayscale
     img = cv.imread('data/r3.jpg', 0)
-    cv.imshow('image', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+
+    # cv.namedWindow('image', cv.WINDOW_NORMAL)
+    # cv.imshow('image', img)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+
+    cv.imwrite('data/r3_copy.png', img)
 
