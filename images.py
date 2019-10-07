a56ef7b2637675ef27a048b98611dbcee81f90e3
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-07 21:36:55 +0300

    moved images tutorial to images.py

---
+++ images.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+if __name__ == '__main__':
+    # Load an color image in grayscale
+    img = cv.imread('data/r3.jpg', cv.IMREAD_COLOR)
+    img = img[..., ::-1]  # converting BGR → RGB colors
+    plt.imshow(img, interpolation='bicubic')
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
