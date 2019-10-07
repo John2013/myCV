4b03128aaa74b7bb786ba3248fbbc77e206e904a
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-07 21:35:20 +0300

    show a color image

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     # Load an color image in grayscale
-    img = cv.imread('data/r3.jpg', 0)
-    plt.imshow(img, cmap='gray', interpolation='bicubic')
+    img = cv.imread('data/r3.jpg', cv.IMREAD_COLOR)
+    img = img[..., ::-1]  # converting BGR → RGB colors
+    plt.imshow(img, interpolation='bicubic')
     plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
     plt.show()
