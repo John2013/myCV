6cbd83fda3363c131be27b24d9ebf56ff1217ee7
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-07 21:14:32 +0300

    exit or save on keys

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import cv2 as cv
 if __name__ == '__main__':
     # Load an color image in grayscale
     img = cv.imread('data/r3.jpg', 0)
-
-    # cv.namedWindow('image', cv.WINDOW_NORMAL)
-    # cv.imshow('image', img)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
-
-    cv.imwrite('data/r3_copy.png', img)
-
+    cv.imshow('image', img)
+    k = cv.waitKey(0)
+    if k == 27:  # wait for ESC key to exit
+        cv.destroyAllWindows()
+    elif k == ord('s'):  # wait for 's' key to save and exit
+        cv.imwrite('data/r3_copy.png', img)
+        cv.destroyAllWindows()
