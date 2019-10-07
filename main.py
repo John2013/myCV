102c6f559f3147752abf175f8aabce1daba40add
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-07 21:26:42 +0300

    show an image by plot

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
     # Load an color image in grayscale
     img = cv.imread('data/r3.jpg', 0)
-    cv.imshow('image', img)
-    k = cv.waitKey(0)
-    if k == 27:  # wait for ESC key to exit
-        cv.destroyAllWindows()
-    elif k == ord('s'):  # wait for 's' key to save and exit
-        cv.imwrite('data/r3_copy.png', img)
-        cv.destroyAllWindows()
+    plt.imshow(img, cmap='gray', interpolation='bicubic')
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
