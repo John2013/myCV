b089c8355a8247ee91b89f6252c3960a50ea52f3
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-08 22:17:40 +0300

    face detection

--- images.py
+++ images.py
@@ -1,10 +1,23 @@
 import cv2 as cv
+import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    # Load an color image in grayscale
-    img = cv.imread('data/r3.jpg', cv.IMREAD_COLOR)
-    img = img[..., ::-1]  # converting BGR → RGB colors
-    plt.imshow(img, interpolation='bicubic')
-    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    face_cascade = cv.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+    eye_cascade = cv.CascadeClassifier('data/haarcascade_eye.xml')
+    img = cv.imread('data/face.jpg')
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0),
+                         2)
+    rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    cv.imwrite('data/face_detected.jpg', img)
+    plt.imshow(rgb)
     plt.show()
