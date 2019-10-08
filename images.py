d794941643a1bfc05a9d633e7fdbc39d190659c2
Evgeny Timofeyev <nawasington@ya.ru>
2019-10-08 23:17:01 +0300

    better eyes cascade

--- images.py
+++ images.py
@@ -3,9 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    face_cascade = cv.CascadeClassifier('data/haarcascade_frontalface_default.xml')
-    eye_cascade = cv.CascadeClassifier('data/haarcascade_eye.xml')
-    img = cv.imread('data/face.jpg')
+    face_cascade = cv.CascadeClassifier(
+        'data/haarcascade_frontalface_default.xml')
+    eye_left_cascade = cv.CascadeClassifier(
+        'data/haarcascade_lefteye_2splits.xml')
+    # eye_right_cascade = cv.CascadeClassifier(
+    #     'data/haarcascade_righteye_2splits.xml')
+    img = cv.imread('data/face2.jpg')
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -13,10 +17,24 @@ if __name__ == '__main__':
         cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0),
-                         2)
+        eyes_left = eye_left_cascade.detectMultiScale(roi_gray)
+        # eyes_right = eye_right_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes_left:
+            cv.rectangle(
+                roi_color,
+                (ex, ey),
+                (ex + ew, ey + eh),
+                (0, 255, 0),
+                2
+            )
+        # for (ex, ey, ew, eh) in eyes_right:
+        #     cv.rectangle(
+        #         roi_color,
+        #         (ex, ey),
+        #         (ex + ew, ey + eh),
+        #         (127, 127, 0),
+        #         2
+        #     )
     rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     cv.imwrite('data/face_detected.jpg', img)
     plt.imshow(rgb)
